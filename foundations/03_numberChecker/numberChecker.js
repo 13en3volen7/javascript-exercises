29ab91a813053666ab39adc82c4b297236d28d58
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-03 21:18:50 +0700

    Finish `numberChecker` exercise

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,8 +1,15 @@
 function numberChecker(number) {
-  if (number === 6) {
-    return true;
-  } else {
-    return false;
+  switch (number) {
+    case 1000:
+    case 10:
+      return true;
+
+    case 9:
+    case 6:
+      return false;
+
+    default:
+      return false;
   }
 }
 
