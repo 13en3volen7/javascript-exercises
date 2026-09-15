99980c7adcb0acbb0371ea44a8ec2d5913fe6c5a
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-15 21:46:44 +0700

    Finish `findTheOldest` exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((a, b) => {
+        if (!("yearOfDeath" in a) || !("yearOfDeath" in b)) {
+            return a.yearOfBirth - b.yearOfBirth;
+        }
+        const ageOfA = a.yearOfDeath - a.yearOfBirth;
+        const ageOfB = b.yearOfDeath - b.yearOfBirth;
+        return ageOfB - ageOfA;
+    })[0];
 };
 
 // Do not edit below this line
