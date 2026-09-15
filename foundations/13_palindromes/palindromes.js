b75fdf093fed91ecc998a219dd8d8b3c664a15ea
13en3volen7 <64196796+13en3volen7@users.noreply.github.com>
2026-09-15 21:17:14 +0700

    Finish `palindromes` exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+function removePunctuationAndWordBreaks(string) {
+    let result = "";
+    for (let letter of string) {
+        if ((letter >= 'A' && letter <= 'Z') || 
+        (letter >= 'a' && letter <= 'z') ||
+        (letter >= '0' && letter <= '9')) {
+            result += letter.toLowerCase();
+        }
+    }
+    return result;
+}
 
+const palindromes = function (string) {
+    const cleanString = removePunctuationAndWordBreaks(string);
+    let l = 0, r = cleanString.length - 1;
+    while (l <= r) {
+        if (cleanString.at(l) !== cleanString.at(r)) {
+            return false;
+        }
+        l++; r--;
+    }
+    return true;
 };
 
 // Do not edit below this line
